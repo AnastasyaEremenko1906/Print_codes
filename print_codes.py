660a0a64de7b06a831600262181305c30a9b314a
Nastya666 <91952545+AnastasyaEremenko1906@users.noreply.github.com>
2021-10-07 09:57:24 +0300

    Add comments for user + add error processing if folder is exist

--- print_codes.py
+++ print_codes.py
@@ -2,16 +2,19 @@ import qrcode
 import os
 from helpers import make_list_of_info, list_of_names
 
+try:
+    qr = qrcode.QRCode(version=1, box_size=10, border=4, )
+    number_of_mku = int(input("Введите трехзначный номер МКУ\n"))
+    name_of_folder = 'Codes_for_{}_MKU'.format(number_of_mku)
+    way_to_folder = os.path.expanduser("~\\Desktop\\{}\\".format(name_of_folder))
+    os.mkdir(way_to_folder)
 
-qr = qrcode.QRCode(version=1, box_size=10, border=4,)
-number_of_mku = int(input("Введите трехзначный номер МКУ\n"))
-name_of_folder = 'Codes_for_{}_MKU'.format(number_of_mku)
-way_to_folder = os.path.expanduser("~\\Desktop\\{}\\".format(name_of_folder))
-os.mkdir(way_to_folder)
-
-for text, code_name in zip(make_list_of_info(number_of_mku), list_of_names):
-    qr.add_data(text)
-    qr.make(fit=True)
-    img = qr.make_image()
-    img.save(f'{way_to_folder}' + f'{code_name}'+'.jpg')
-    qr.clear()
+    for text, code_name in zip(make_list_of_info(number_of_mku), list_of_names):
+        qr.add_data(text)
+        qr.make(fit=True)
+        img = qr.make_image()
+        img.save(f'{way_to_folder}' + f'{code_name}' + '.jpg')
+        qr.clear()
+    print(f"Коды по {number_of_mku} МКУ успешно созданы и лежат на рабочем столе")
+except FileExistsError:\
+    print(f"Папка с кодами по {number_of_mku} МКУ уже создана")
